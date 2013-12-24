3e0ca5a49e47ac38a1316078973167a4bf29314d
Ijul Farizul <farizul@gmail.com>
2013-12-24 17:10:27 +0800

    produk

--- intelmlm_modules/Main/guest_main.php
+++ intelmlm_modules/Main/guest_main.php
@@ -7,6 +7,7 @@ include(THEME_LOC."/main_header.php");
 			<div class="tab-control" data-role="tab-control">
                 <ul class="tabs">
                     <li class="active"><a href="#_pengenalan">Pengenalan</a></li>
+                    <li class="active"><a href="#_produk">Produk</a></li>
                     <li><a href="#_langkah_pertama">Langkah Pertama</a></li>
                     <li><a href="#_langkah_kedua">Langkah Kedua</a></li>
                     <li><a href="#_kemudian">Kemudian</a></li>
@@ -17,7 +18,12 @@ include(THEME_LOC."/main_header.php");
              
                 <div class="frames">
                     <div class="frame" id="_pengenalan">
-                        <p><strong>Selamat datang ke Program Galakan Usahawan 1Malaysia</strong></p>
+                        <h4 class="padding10">Selamat datang ke Program Galakan Usahawan 1Malaysia</h4>
+                        <p></p>
+                    </div>
+                    <div class="frame" id="_produk">
+                        <h4 class="padding10">Produk dan perkhidmatan</h4>
+                        <p>Kami mempunyai pelbagai jenis produk dan perkhidmatan seperti 'Referral Link', 'Facebook API', 'SMS Notification' dan sebagainya.</p>
                     </div>
                     <div class="frame" id="_langkah_pertama">
                         <h4 class="padding10">Pilih produk atau perkhidmatan yang anda ingin</h4>
@@ -25,8 +31,12 @@ include(THEME_LOC."/main_header.php");
                     <div class="frame" id="_langkah_kedua">
                         <h4 class="padding10">Buat pesanan dan pembayaran kepada peniaga-peniaga</h4>
                     </div>
-                    <div class="frame" id="_kemudian">...</div>
-                    <div class="frame" id="_dan_seterusnya">...</div>
+                    <div class="frame" id="_kemudian">
+                        <h4 class="padding10">Promosikan produk atau perkhidmatan anda kepada rakan-rakan</h4>
+                    </div>
+                    <div class="frame" id="_dan_seterusnya">
+                        <h4 class="padding10"></h4>
+                    </div>
                     <div class="frame" id="_page_3">...</div>
                 </div>
             </div>
