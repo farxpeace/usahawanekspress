e7f772b2903450f3bc72d74b288367f60a821299
Ijul Farizul <farizul@gmail.com>
2014-01-08 13:21:15 +0800

    adodb

--- intelmlm_library/model.php
+++ intelmlm_library/model.php
@@ -1,17 +1,40 @@
 <?php
 class Model {
+    public $config;
     public $db;
+    
+    public $table_;
     
     function __construct($model){
         $this->db = NewADOConnection('mysql');
         $this->db->Connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
         $this->db->SetFetchMode(ADODB_FETCH_ASSOC);
         
+        $this->config = new stdClass;
+        $this->config->check_table = true;
+        $this->config->create_table = true;
+        
         foreach ($model as $i => $modelname){
             $xml = simplexml_load_file(FOLDER_MODULES.'/'.$modelname.'/Model.xml');
+            $modelname = strtolower($modelname);
             $arr = (array) $xml;
-            $this->{strtolower($modelname)} = $arr;
-            var_dump($this->table_exists('iasdasd'));
+            $self = $this->{$modelname} = $xml;
+            //$self = $this->{$modelname};
+            
+            
+            //$self->xml_obj = $xml;
+            
+            
+            //check table exists
+            if($this->config->check_table){
+                $self->xml->table->isExists = $this->table_exists($self->attributes()->{'name'});
+            }
+            if($this->config->create_table){
+                
+            }
+            //echo '<pre>';
+            //print_r($this->{$modelname});
+            
         }
     }
     
@@ -20,9 +43,11 @@ class Model {
         $res = $this->db->Execute("
             SELECT COUNT(*) AS count 
             FROM information_schema.tables 
-            WHERE table_name = '$tablename'
+            WHERE table_schema ='".$this->db->databaseName."' AND table_name = '$tablename'
         ");
-        return ($res ? true : false);
+        
+        
+        return ($res->fields['count'] ? true : false);
     }
 }
 
